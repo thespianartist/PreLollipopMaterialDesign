07045efa187a43073a2f5b8f94d3560c930d7f3a
Uriel Rodolfo Ramirez Moreno <topzihxc@gmail.com>
2014-12-08 16:21:28 -0600

    Testing on Android 5.0 and Android 4.4 OK

--- app/src/main/java/la/mejorando/prelollipopmaterialdesign/MainActivity.java
+++ app/src/main/java/la/mejorando/prelollipopmaterialdesign/MainActivity.java
@@ -1,10 +1,18 @@
 package la.mejorando.prelollipopmaterialdesign;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import la.mejorando.prelollipopmaterialdesign.adapters.PusheenAdapter;
+import la.mejorando.prelollipopmaterialdesign.models.Pusheen;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,9 +22,52 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Configurando que el Toolbar como ActionBarActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_my_toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
+
+        //Creando el Dataset, recuerda que esto no siempre es hardcodeado
+        ArrayList<Pusheen> pusheens = new ArrayList<>();
+
+        Pusheen pusheen = new Pusheen();
+        pusheen.setId(1);
+        pusheen.setName("Pusheen");
+        pusheen.setPasTime("Blogger");
+        pusheens.add(pusheen);
+
+        Pusheen pusheen2 = new Pusheen();
+        pusheen2.setId(2);
+        pusheen2.setName("Pusheen");
+        pusheen2.setPasTime("Sculpor");
+        pusheens.add(pusheen2);
+
+        Pusheen pusheen3 = new Pusheen();
+        pusheen3.setId(3);
+        pusheen3.setName("Stormy");
+        pusheen3.setPasTime("Mage");
+        pusheens.add(pusheen3);
+
+        Pusheen pusheen4 = new Pusheen();
+        pusheen4.setId(4);
+        pusheen4.setName("Pusheen");
+        pusheen4.setPasTime("Tribute");
+        pusheens.add(pusheen4);
+
+        Pusheen pusheen5 = new Pusheen();
+        pusheen5.setId(5);
+        pusheen5.setName("Stormy");
+        pusheen5.setPasTime("adventurer");
+        pusheens.add(pusheen5);
+
+        //Fin de nuestro dataset
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new PusheenAdapter(pusheens, R.layout.row));
+        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
 
     }
 
