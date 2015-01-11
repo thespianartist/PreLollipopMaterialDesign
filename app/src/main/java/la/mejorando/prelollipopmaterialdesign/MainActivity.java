e473bf51d1a96488c894068234f08575662e3387
Uriel Rodolfo Ramirez Moreno <topzihxc@gmail.com>
2015-01-11 00:16:42 -0600

    SlidingTab with Fragments

--- app/src/main/java/la/mejorando/prelollipopmaterialdesign/MainActivity.java
+++ app/src/main/java/la/mejorando/prelollipopmaterialdesign/MainActivity.java
@@ -1,26 +1,30 @@
 package la.mejorando.prelollipopmaterialdesign;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
-
-import la.mejorando.prelollipopmaterialdesign.adapters.PusheenAdapter;
-import la.mejorando.prelollipopmaterialdesign.models.Pusheen;
+import la.mejorando.prelollipopmaterialdesign.adapters.FragmentPagerAdapter;
+import la.mejorando.prelollipopmaterialdesign.views.SlidingTabLayout;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setToolbar();
+        setTabsandPager();
+
+    }
+
+
+    public void setToolbar(){
 
         //Configurando que el Toolbar como ActionBar
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_my_toolbar);
@@ -28,84 +32,16 @@ public class MainActivity extends ActionBarActivity {
         toolbar.setTitle("I am Pusheen");
         setSupportActionBar(toolbar);
 
-
-        //Creando el Dataset, recuerda que esto no siempre es hardcodeado
-        ArrayList<Pusheen> pusheens = new ArrayList<>();
-
-        Pusheen pusheen = new Pusheen();
-        pusheen.setId(1);
-        pusheen.setName("Pusheen");
-        pusheen.setPasTime("Blogger");
-        pusheens.add(pusheen);
-
-        Pusheen pusheen2 = new Pusheen();
-        pusheen2.setId(2);
-        pusheen2.setName("Pusheen");
-        pusheen2.setPasTime("Sculpor");
-        pusheens.add(pusheen2);
-
-        Pusheen pusheen3 = new Pusheen();
-        pusheen3.setId(3);
-        pusheen3.setName("Stormy");
-        pusheen3.setPasTime("Mage");
-        pusheens.add(pusheen3);
-
-        Pusheen pusheen4 = new Pusheen();
-        pusheen4.setId(4);
-        pusheen4.setName("Pusheen");
-        pusheen4.setPasTime("Tribute");
-        pusheens.add(pusheen4);
-
-        Pusheen pusheen5 = new Pusheen();
-        pusheen5.setId(5);
-        pusheen5.setName("Stormy");
-        pusheen5.setPasTime("adventurer");
-        pusheens.add(pusheen5);
-
-        Pusheen pusheen6 = new Pusheen();
-        pusheen6.setId(1);
-        pusheen6.setName("Pusheen");
-        pusheen6.setPasTime("Blogger");
-        pusheens.add(pusheen6);
-
-        Pusheen pusheen7 = new Pusheen();
-        pusheen7.setId(2);
-        pusheen7.setName("Pusheen");
-        pusheen7.setPasTime("Sculpor");
-        pusheens.add(pusheen7);
-
-        Pusheen pusheen8 = new Pusheen();
-        pusheen8.setId(3);
-        pusheen8.setName("Stormy");
-        pusheen8.setPasTime("Mage");
-        pusheens.add(pusheen8);
-
-
-        //Fin de nuestro dataset
-
-        //Configuración del recyclerView, con nuestro dataSet y nuestro Adapter
-
-        //Aqui buscamos el Widget recyclerView
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        //A pesar del tamaño de la pantalla, asegurar que los elementos se vean siempre igual.
-        recyclerView.setHasFixedSize(true);
-
-        //Configuramos la animación por defecto
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        //Ahora le pasamos nuestro dataset y la forma en que se vera cada fila.
-        recyclerView.setAdapter(new PusheenAdapter(pusheens, R.layout.row));
-
-        //Importante, ahora podemos indicar si mostrar los elementos como fila o como grilla, en este
-        //caso setLayoutManager() esta  configurado como grilla de una columna, es por es eso que se ve
-        //como fila, recordar que el mismo resultado se obtiene con
-        // recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
-
-
     }
 
+    public void setTabsandPager(){
+
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()));
+        SlidingTabLayout mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setViewPager(mViewPager);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -128,4 +64,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/la/mejorando/prelollipopmaterialdesign/adapters/FragmentPagerAdapter.java
@@ -0,0 +1,41 @@
+package la.mejorando.prelollipopmaterialdesign.adapters;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import la.mejorando.prelollipopmaterialdesign.fragments.HelloFragment;
+
+
+public class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
+
+    private final String [] TITLES = {"Home", "Pusheen", "Stormy"};
+    private final int FRAGMENT_COUNT = 3;
+
+
+    public FragmentPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public int getCount() {
+        return FRAGMENT_COUNT;
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TITLES[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+          return new HelloFragment();
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+}
